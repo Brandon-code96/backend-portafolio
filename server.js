9c5f89c62a1e736b5f8fb392a63a3f68267bb766
Brandon Ubillus <brandonubillusbarraza@gmail.com>
2024-10-30 19:29:55 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const path = require('path')
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, '/public/')));
+
+
+app.post('/send', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: subject,
+    text: `Nombre: ${name}\nCorreo: ${email}\nMensaje: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Correo enviado exitosamente');
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
